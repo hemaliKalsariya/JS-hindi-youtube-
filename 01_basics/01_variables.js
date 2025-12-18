826824f0ec581103a2ffe376ff1b3ab2e89427bb
hemaliKalsariya <hemukalsariya7705@gmail.com>
2025-12-18 09:29:47 +0000

    const change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14452
+let accounEmail = "hemali@gmail.com"
+var accountPassword = "1818"    
+accountCity = "Jaypur"
+
+
+/*
+Prfer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+

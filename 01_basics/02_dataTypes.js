885a7ac6e80f0c1d1dbee9b9e94eb4a9e65bad36
hemaliKalsariya <hemukalsariya7705@gmail.com>
2025-12-18 10:22:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(3+3) // We are using node js, not broweser
+
+console.log(3 + 3);
+
+console.log("Kalsariya");
+
+let name = "hemali"
+let age = "20"
+
+
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+// boolean => true/false
+// null =>  standelone value
+//undefrined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "51518");

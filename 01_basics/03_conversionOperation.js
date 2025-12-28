de59b98883af0b5b495ea59e7ec72613fea1d3f0
hemaliKalsariya <hemukalsariya7705@gmail.com>
2025-12-28 11:57:25 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)   
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+//"33" => 33
+//"33acb" => NaN
+// true => 1 ; false=> 0
+
+
+let isLoggedIn = "Hemali"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 
+// "" => false
+// "Hemali" => true
+
+let age = 21
+let stringNumber = String(age)
+console.log(stringNumber);
+console.log(typeof stringNumber);

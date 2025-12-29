5da5a7bf18d7b8b1f13015512f4da66549d392ed
hemaliKalsariya <hemukalsariya7705@gmail.com>
2025-12-29 09:20:20 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "true"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)   
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
 
 
 //"33" => 33
@@ -15,7 +15,7 @@ console.log( valueInNumber);
 
 let isLoggedIn = "Hemali"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1=> true; 
 // "" => false
@@ -23,5 +23,33 @@ console.log(booleanIsLoggedIn);
 
 let age = 21
 let stringNumber = String(age)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+//********************************Operations************************************ 
+let value = 3 
+let nagValue = -value
+console.log(nagValue);
+
+let str1 = "Hallo"
+let str2 = "  ms."
+let str3 =  "  Kalsariya"
+let str4 = str1+str2+str3
+console.log(str4);
+
+console.log("1"+ 2 );
+console.log(1+ 2 );
+console.log(1+ "2" );
+console.log("1"+ 2 + 3 );
+console.log(1+ 2 + "3" );
+
+
+console.log(+true);
+

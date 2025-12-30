b472f8838f1c5d8ecbe8eb7802cf517affd8aa92
hemaliKalsariya <hemukalsariya7705@gmail.com>
2025-12-30 17:28:23 +0000

    dataTypes

---
+++ 01_basics/dataTypes.summary.js
@@ -0,0 +1,25 @@
+// primitive
+
+// 7 types : string, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+// in 
+// Array, Objects, Functions
+
+const score = 100
+const scoreValue = 120.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(score, scoreValue, isLoggedIn, outsideTemp, userEmail, id, anotherId );
+
+
+const myfunction = function  () {
+    console.log("hello world");
+    
+}
+

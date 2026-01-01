378166eab26e1966f6d4ec640caa76564aaf0b5d
hemaliKalsariya <hemukalsariya7705@gmail.com>
2026-01-01 06:51:05 +0000

    math and num

--- 01_basics/06_nums,and,math.js
+++ 01_basics/06_nums,and,math.js
@@ -2,11 +2,30 @@ const score = 100
 console.log(score);
 
 const balance = new Number(2000)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toFixed(5));
+//console.log(balance.toFixed(5));
 
 const Threrty = 500000000
-console.log(Threrty.toLocaleString('en-IN'));
+//console.log(Threrty.toLocaleString('en-IN'));
 
 //*****************************Maths******************************************//
+
+console.log(Math);
+console.log(Math.abs(-58));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.min(4, 5, 1, 6));
+console.log(Math.max(4, 5, 1, 6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20
+const max = 50
+
+//console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+

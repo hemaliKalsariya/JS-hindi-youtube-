dd9a0e47d5cc14c773cdfe6abcc38c5841bd2391
hemaliKalsariya <hemukalsariya7705@gmail.com>
2026-01-01 15:03:44 +0000

    arrey

---
+++ 02_Basics/01_arreys.js
@@ -0,0 +1,36 @@
+//arreys
+
+const myArr = [0, 1, 2, 3, 4, ]
+// console.log(myArr[4]);
+
+
+//Arrey methods
+
+// myArr.push(5)
+// myArr.pop()
+
+// myArr.unshift(8)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(newArr);
+
+//slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log("B", myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
+
+

786b68dbd0c7b3be466f0270f67144e77a1724b1
hemaliKalsariya <hemukalsariya7705@gmail.com>
2026-01-05 13:41:31 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,52 @@ let a = 500                         //global scope
 if (true) {                                            //block scope
     let a = 20                                          //block scope   
     const b = 30                                        //block scope
-    console.log("price:", a, b);                        //block scope
+    // console.log("price:", a, b);                        //block scope
     
 }                                                       //block scope            
 
-console.log(a);                             //global scope
+//console.log(a);                             //global scope
                      
+function one(){
+    const username = "hemali"  
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+       
+    }
+        // console.log(website);
+
+    two()
+                 
+}
+
+//  one()
+
+if (true){
+    const username = "Hemali"
+    if (username === "Hemali"){
+        const website = "  YouTube"
+        // console.log(username + website);     
+    }
+    //console.log(website);
+
+}
+// console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++ interesting example ++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5));
+
+

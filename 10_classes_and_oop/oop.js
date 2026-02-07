66fae544d5bc6709a1d4f8af9cb65776fed3e2e1
hemaliKalsariya <hemukalsariya7705@gmail.com>
2026-02-07 17:34:43 +0000

    OOP

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -8,14 +8,29 @@ const user = {
         //  console.log(`userName: ${this.username}`);
            console.log(this);
         
-       
-        
     }
 
-
-        
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
-console.log(this);
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function() {
+        console.log(`Hello ${this.username}`);
+    }
+
+    return this;
+}
+
+const userOne = new  User("Hemali", 10, true);
+const userTwo = new User("Kalsari", 5, false);
+
+console.log(userOne);
+console.log(userTwo.constructor);
